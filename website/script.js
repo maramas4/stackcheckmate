191b0f240264e9a08a85b7a2358ab66ca36354ae
chisom life eke <chisomlifeeke@gmail.com>
2025-10-12 14:43:04 +0100

    Merged remote main with local Smart Installer files

--- website/script.js
+++ website/script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("StackCheckMate Landing Page Loaded ✅");
 });
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
